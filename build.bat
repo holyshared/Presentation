640255fbfb7e73fcf5f2108eeb583f39432ed029
Noritaka Horio <holy.shared.design@gmail.com>
2012-01-21 20:11:05 +0900

    add compressed files.

--- build.bat
+++ build.bat
@@ -3,3 +3,8 @@ php packager build Presentation/Presentation Presentation/FullScreen Presentatio
 php packager build Presentation/Presentation Presentation/FullScreen Presentation/Filter Presentation/Helper Presentation/Helper.Swipe Presentation/Helper.Keyboard Presentation/Helper.Page Presentation/Helper.Controller > C:\var\www\git\Presentation\Build\presentation-ipad.js
 php packager build Presentation/FullScreen Presentation.Bootstrap/Filter Presentation.Bootstrap/Keyboard Presentation.Bootstrap/Page Presentation.Bootstrap/Controller > C:\var\www\git\Presentation\Build\presentation-bootstrap-standard.js
 php packager build Presentation/FullScreen Presentation.Bootstrap/Filter Presentation.Bootstrap/Keyboard Presentation.Bootstrap/Page Presentation.Bootstrap/Controller Presentation.Bootstrap/Swipe > C:\var\www\git\Presentation\Build\presentation-bootstrap-ipad.js
+
+java -jar C:\var\www\tools\yuicompressor\build\yuicompressor-2.4.6.jar -o C:\var\www\git\Presentation\Build\presentation-standard-compressed.js C:\var\www\git\Presentation\Build\presentation-standard.js
+java -jar C:\var\www\tools\yuicompressor\build\yuicompressor-2.4.6.jar -o C:\var\www\git\Presentation\Build\presentation-ipad-compressed.js C:\var\www\git\Presentation\Build\presentation-ipad.js
+java -jar C:\var\www\tools\yuicompressor\build\yuicompressor-2.4.6.jar -o C:\var\www\git\Presentation\Build\presentation-bootstrap-standard-compressed.js C:\var\www\git\Presentation\Build\presentation-bootstrap-standard.js
+java -jar C:\var\www\tools\yuicompressor\build\yuicompressor-2.4.6.jar -o C:\var\www\git\Presentation\Build\presentation-bootstrap-ipad-compressed.js C:\var\www\git\Presentation\Build\presentation-bootstrap-ipad.js
